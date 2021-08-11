8856fca30b782675f4e42c31de63c008c56b2996
Ole Max Haaland <omhaala@stud.ntnu.no>
2021-08-11 18:24:19 +0200

    .

--- setup.sh
+++ setup.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 #This script assumes that it is run from the git repo
 
-LAYOUTDIR=/usr/share/X11/xbk/symbols/
+LAYOUTDIR=/usr/share/X11/xkb/symbols/
 
 cp ctrl $LAYOUTDIR
-cp us_personal $LAYOUTDIR
- 
-setxkbmap -layout us_personal -variant colemak
+cp personal $LAYOUTDIR
+
+
+
+
+VDRIVE=/media/ole/ADVANTAGE2
+mkdir $VDRIVE
+mount /dev/disk/by-label/ADVANTAGE2 /media/ole/ADVANTAGE2
+
+cp qwerty.txt $VDRIVE/active/
+
+umount /media/ole/ADVANTAGE2
+
+#Ask for usr input
+#setxkbmap -layout personal -variant colemak
+
+
+
